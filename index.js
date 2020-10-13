90954c440a3d9a4cb617f2bb94db3b72878bf845
tekilit@gmail.com <67770867+tekilit@users.noreply.github.com>
2020-10-13 21:23:46 +0200

    arr+fun7

---
+++ index.js
@@ -0,0 +1,48 @@
+// 1.- Crear variable de nombre arrayVacio cuyo valor sea un array vacío
+
+var arrayVacio = [ ]
+
+console.log(arrayVacio)
+
+// 2.- Crear variable de nombre arrayNumeros declarada con un array de números del 0 al 9 (0, 1, 2...)
+
+var arrayNumeros = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+console.log(arrayNumeros)
+
+// 3.- Crear variable de nombre arrayNumerosPares declarada con un array con los números pares del 0 al 9 (considerando al 0 par)
+
+var arrayNumerosPares = [ 0, 2, 4, 6, 8 ]
+
+console.log(arrayNumerosPares)
+
+// 4.- Crear variable de nombre arrayBidimensional declarada con valor array [[0, 1, 2], ['a', 'b', 'c']]
+
+var arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']]
+
+console.log(arrayBidimensional)
+
+// Funciones
+
+// 5.- Crea la función suma que acepte como argumento dos números y devuelva el resultado de su suma
+
+ffunction suma(a, b) {
+    var suma = 2 + 8
+  return suma
+ }
+ 
+ console.log(suma())
+
+ // 6.- Crea la función potenciacion que acepte como argumento dos números y devuelva el resultado de elevar el primero(a) al segundo(b) (a^b)
+
+ function suma(a, b) {
+    var suma = 2 ^ 8
+  return suma
+ }
+ 
+ console.log(suma())
+
+ //  7.- Crea la función separarPalabras que acepte como argumento un string y devuelva un array de palabras 'hola mundo' => [hola, mundo]
+
+ 
+
