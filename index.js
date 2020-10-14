68f8afe01d3fccb1e569d558eca68bf4e3dc04a9
tekilit@gmail.com <67770867+tekilit@users.noreply.github.com>
2020-10-14 09:53:13 +0200

    ejercicio 7 + enunciados hasta el 14

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ console.log(arrayBidimensional)
 
 // 5.- Crea la función suma que acepte como argumento dos números y devuelva el resultado de su suma
 
-ffunction suma(a, b) {
+function suma(a, b) {
     var suma = 2 + 8
   return suma
  }
@@ -45,4 +45,24 @@ ffunction suma(a, b) {
  //  7.- Crea la función separarPalabras que acepte como argumento un string y devuelva un array de palabras 'hola mundo' => [hola, mundo]
 
  
+ function separarPalabras(hola,mundo) {
+  var separarPalabras = ['hola', 'mundo']
+return separarPalabras
+   }
 
+console.log(separarPalabras())
+
+// 8.- Crea la función repetirString que acepte como argumento un string y un número y devuelva un string que sea el resultado de concatenar el primer string el número dado de veces
+
+ // 9.- Crea la función esPrimo que acepte como argumento un número y devuelva true si es primo y false si no lo es
+
+// Mezclando arrays y funciones
+ // 10.- Crear la función ordenarArray que acepta como argumento un array de números y devuelva un array ordenado de menor a mayor
+
+ // 11.- Crear la función obtenerPares que acepta como argumento un array de números y devuelva un array con los elementos pares
+
+ // 12.- Crear la función pintarArray que acepte como argumento un array y devuelva una cadena de texto Array entrada: [0, 1, 2] String salida: '[0, 1, 2]'
+
+ // 13.- Crear la función arrayMapi que acepte como argumento un Array y una función y devuelva un array en el que se haya aplicado la función a cada elemento del array
+
+ // 14.- Crear la función eliminarDuplicados que acepte como argumento un array y devuelva un array en el que se hayan eliminado los duplicados
